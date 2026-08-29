fe3c477ff0f64daa329bf1244abaa01e569d9d30
Dian Khaerunia Azzahra <dianazzahra1314@gmail.com>
2026-08-29 12:33:47 +0700

    Menambahkan praktikum minggu kedua

---
+++ PraktikumPBO/src/main/java/dian/PraktikumPBOWeek2/PerkenalanSederhana.java
@@ -0,0 +1,19 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ */
+
+package dian.PraktikumPBOWeek2;
+
+/**
+ *
+ * @author ThinkPad T14 G2
+ */
+public class PerkenalanSederhana {
+
+    public static void main(String[] args) {
+        System.out.println("Nama    : Dian Khaerunia Azzahra");
+        System.out.println("NPM     : 2505060039");
+        System.out.println("Alamat  : Saragan, Banyurojo, Mertoyudan, Magelang");
+        System.out.println("No. Hp  : 085747086139");
+    }
+}
